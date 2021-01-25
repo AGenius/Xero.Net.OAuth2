495635b2e6e8acb89943cb01ae5ab2fe05251cbf
AGenius <github@enigmaware.co.uk>
2021-01-25 14:30:51 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xero.Net")]
+[assembly: AssemblyDescription("A new OAuth2 for .NET Framework. Derived from the Xero-NetStandard. here https://github.com/XeroAPI/Xero-NetStandard")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AGenius")]
+[assembly: AssemblyProduct("Xero.Net")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3875286-3898-408d-9bc3-d4c50e177dfb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2021.0125.1428")]
+[assembly: AssemblyFileVersion("1.2021.0125.1428")]
